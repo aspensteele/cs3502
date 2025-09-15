84d700640d4c7611caac9113cd4f54a822ea359c
aspensteele <aspen@steele-online.com>
2025-09-15 13:39:55 -0400

    adding signal enhancements to consumer

--- assignment2/part1/consumer.c
+++ assignment2/part1/consumer.c
@@ -5,8 +5,25 @@
 #include <getopt.h>
 #include <time.h>
 
+//global variables for signal handlers
+volatile sig_atomic_t shutdown_flag = 0;
+volatile sig_atomic_t stats_flag = 0;
+
+//statistics tracking
+volatile sig_atomic_t lines_processed = 0;
+volatile sig_atomic_t chars_processed = 0;
+clock_t start_time; 
+
+void handle_sigint(int sig) {
+	shutdown_flag = 1;
+}
+
+void handle_sigusr1(int sig) {
+	stats_flag = 1;
+} 
+
 int main(int argc, char *argv[]) {
-	int max_lines = -1; // -1 means uinlimited
+	int max_lines = -1; // -1 means unlimited
 	int verbose = 0; 
 	char opt; 
 
@@ -14,6 +31,10 @@ int main(int argc, char *argv[]) {
 	int char_count = 0;
 	char buffer[1024]; //buffer for reading lines
 
+	// Set yp signal handlers
+	struct sigaction sa_int, sa_usr1; 
+
+		
 	//Parse arguments
 	while ((opt = getopt(argc, argv, "n:v")) != -1) {
 	    switch (opt) {
@@ -32,7 +53,14 @@ int main(int argc, char *argv[]) {
 		return 1;
 		}
 	}
+	
+	//register signal handlers
+	struct sigaction sa;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);	
 
+	clock_t start = clock(); // start timer for performance metrics
 
 	while (fgets(buffer, sizeof(buffer), stdin) != 	NULL) {
 	   line_count++;
@@ -42,7 +70,16 @@ int main(int argc, char *argv[]) {
 	   if(verbose) {
 		printf("Line  %d: %s", line_count, buffer);
 	   }
-
+	   // if sigusr1 received, print stats
+	  if (stats_flag) {
+		fprintf(stderr, " Current Stats: Lines=%d, Chars=%d\n",
+				line_count, char_count);
+		stats_flag = 0; //reset
+	    }
+	  // If SIGINT received, shutdown gracefully
+	  if (shutdown_flag) {
+	 fprintf(stderr,"Shutdown signal received. Exiting...\n");
+	}
 	  if (max_lines != -1 && line_count >= max_lines) {
 		if (verbose) {
 		fprintf(stderr, "Reached maximum lines limit (%d)\n", max_lines);
@@ -51,9 +88,21 @@ int main(int argc, char *argv[]) {
 	}
     }
 
+
+	//end timer and performance stats
+	clock_t end = clock(); 
+	double cpu_time = ((double)(end - start)) // CLOCKS_PER_SEC;
+
 	fprintf(stderr, "Lines: %d\n", line_count);
 	fprintf(stderr, "Characters: %d\n", char_count);
-
+	
+	if (cpu_time > 0) {
+	double lines_per_sec = line_count / cpu_time;
+	double bytes_per_sec = char_count / cpu_time;
+	fprintf(stderr, "Throughout: %.2f lines/sec, 5.2f bytes/sec\n",	
+	 	lines_per_sec, bytes_per_sec);
+	}
+	
 	return 0;
 } 
 
