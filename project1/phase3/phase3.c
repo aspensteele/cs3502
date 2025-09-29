5d03856bfaa659ad4da6924e6a4134664720ab8a
aspensteele <aspen@steele-online.com>
2025-09-29 12:33:23 -0400

    add given code

--- project1/phase3/phase3.c
+++ project1/phase3/phase3.c
@@ -1,79 +1,48 @@
-//testing for  github
+//Phase 3: Deadlock Creation
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <string.h> 
+#include <errno.h>
 
 typedef struct {
-    int account_id;
-    double balance;
-    pthread_mutex_t lock;
-} Account;
+	int account_id;
+	double balance;
+	int transaction_count;
+	pthread_mutex_t lock; // mutex for thus account
+} Account; 
 
-Account accounts[2]; // Two accounts for deadlock demo
 
-typedef struct {
-    int from_id;
-    int to_id;
-    double amount;
-} TransferArgs;
 
-void* transfer_thread(void* arg) {
-    TransferArgs* t = (TransferArgs*)arg;
-    printf("Thread %ld: Attempting transfer from %d to %d\n",
-           pthread_self(), t->from_id, t->to_id);
+void transer(int from_id, int to_id, double amount) {
+	printf("Thread %ld: Attempting transfer from %d to %d\n",
+	      pthread_self()m, from_id, to_id); 
 
-    pthread_mutex_lock(&accounts[t->from_id].lock);
-    printf("Thread %ld: Locked account %d\n", pthread_self(), t->from_id);
+  	pthread_mutex_lock(&accounts[from_id].lock);
+	printf("Thread %ld: Locked account %d\n", pthread_self(), from_id);
+	
+	//simulate processing delay - gives other thread time to create deadlock
+	usleep(100); //sleep for 100 microseconds
 
-    usleep(100); // Simulate processing delay
+	printf("Thread %ld: Waiting for account %d\n", pthread_self(), to_id);
+	pthread_mutex_lock(&accounts[to_id].lock);
 
-    printf("Thread %ld: Waiting for account %d\n", pthread_self(), t->to_id);
-    pthread_mutex_lock(&accounts[t->to_id].lock);
+	// If we get here, no deadlock occured this time.
+	accounts[from_id].balance -= amount;
+	accounts[to_id].balance += amount; 
 
-    // Perform transfer
-    accounts[t->from_id].balance -= t->amount;
-    accounts[t->to_id].balance += t->amount;
+	pthread_mutex_unlock(&accounts[to_id].lock);
+	pthread_mutex_unlock(&accounts[from_id].lock)ll
 
-    pthread_mutex_unlock(&accounts[t->to_id].lock);
-    pthread_mutex_unlock(&accounts[t->from_id].lock);
-
-    printf("Thread %ld: Transfer complete from %d to %d\n",
-           pthread_self(), t->from_id, t->to_id);
-
-    return NULL;
 }
 
 int main() {
-    pthread_t t1, t2;
-
-    // Initialize accounts
-    for (int i = 0; i < 2; i++) {
-        accounts[i].account_id = i;
-        accounts[i].balance = 1000.0;
-        pthread_mutex_init(&accounts[i].lock, NULL);
-    }
-
-    // Thread 1: transfer 100 from account 0 -> 1
-    TransferArgs args1 = {0, 1, 100};
-    // Thread 2: transfer 200 from account 1 -> 0
-    TransferArgs args2 = {1, 0, 200};
-
-    pthread_create(&t1, NULL, transfer_thread, &args1);
-    pthread_create(&t2, NULL, transfer_thread, &args2);
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-
-    printf("\nFinal balances:\n");
-    for (int i = 0; i < 2; i++) {
-        printf("Account %d: %.2f\n", i, accounts[i].balance);
-    }
-
-    // Cleanup mutexes
-    for (int i = 0; i < 2; i++) {
-        pthread_mutex_destroy(&accounts[i].lock);
-    }
-
-    return 0;
+	
+	transfer(1,2,50)
+	transfer(2,1,50)
+	
+	
+return 0; 
 }
