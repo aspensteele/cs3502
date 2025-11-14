fce7945d4ab37f27d13b61129566a3296291aba0
Aspen Steele <142335800+aspensteele@users.noreply.github.com>
2025-11-14 09:43:23 -0500

    Update buffer.h

--- assignment3/buffer.h
+++ assignment3/buffer.h
@@ -1,22 +1,23 @@
-	// Aspen Steele
-	// CS 3502 - Section 04 - Assignment 3
+// Aspen Steele
+// CS 3502 - Section 04 - Assignment 3
 
-	#ifndef BUFFER_H
-	#define BUFFER_H
+#ifndef BUFFER_H
+#define BUFFER_H
 
-	#define BUFFER_SIZE 10
-	#define SHM_KEY 0x1234
+#define BUFFER_SIZE 10
+#define SHM_KEY 0x1234
 
-	typedef struct {
-		int value; // Data value
-		int producer_id; // Which producer created this
-	} item_t;
+typedef struct {
+	int value; // Data value
+	int producer_id; // Which producer created this
+} item_t;
 
-	typedef struct {
-		item_t buffer[BUFFER_SIZE];
-		int head; // Next write position
-		int tail; // Next read position
-		int count; // Current items in buffer
-	} shared_buffer_t;
+typedef struct {
+	item_t buffer[BUFFER_SIZE];
+	int head; // Next write position
+	int tail; // Next read position
+	int count; // Current items in buffer
+} shared_buffer_t;
 
-	#endif
+
+#endif
